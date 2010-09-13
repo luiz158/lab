57d97ef2e6aa13dcce37aaf9d201adbadc8449f7
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2010-09-13 19:44:19 +0000

    Finalização do exemplo JSF
    git-svn-id: http://zyc.googlecode.com/svn/lab@14 dea8d998-f866-49cc-146f-adbbfafdc004

---
+++ jee6/examples/jsf/registering/src/main/java/examples/Register.java
@@ -0,0 +1,24 @@
+package examples;
+
+public class Register {
+
+	private String name;
+
+	private String cellPhone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+}
---
+++ jee6/examples/jsf/registering/src/main/java/examples/RegisterBean.java
@@ -0,0 +1,64 @@
+package examples;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class RegisterBean {
+
+	public Register register;
+
+	public Register getRegister() {
+		return register;
+	}
+
+	@PostConstruct
+	public void init() {
+		register = new Register();
+	}
+
+	public String save() {
+		validateName();
+		validateCellPhone();
+
+		String outcome = null;
+
+		if (!hasQueuedMessages()) {
+			outcome = "/result.xhtml";
+		}
+
+		return outcome;
+	}
+
+	private void validateName() {
+		if (register.getName() == null || register.getName().trim().equals("")) {
+			addMessage("name", "O nome deve ser preenchido");
+		}
+	}
+
+	private void validateCellPhone() {
+		if (register.getCellPhone() == null || register.getCellPhone().trim().equals("")) {
+			addMessage("cellPhone", "O número do celular deve ser preenchido");
+		}
+
+		Pattern p = Pattern.compile("^(\\(\\d{2}\\) )?\\d{4}-\\d{4}$");
+		Matcher m = p.matcher(register.getCellPhone());
+		if (!m.matches()) {
+			addMessage("cellPhone", "O número do celular deve ter o seguinte formato: (XX) XXXX-XXXX");
+		}
+	}
+
+	private void addMessage(String id, String message) {
+		FacesMessage facesMessage = new FacesMessage(message);
+		FacesContext.getCurrentInstance().addMessage(id, facesMessage);
+	}
+
+	private boolean hasQueuedMessages() {
+		return FacesContext.getCurrentInstance().getMessageList().size() > 0;
+	}
+}
