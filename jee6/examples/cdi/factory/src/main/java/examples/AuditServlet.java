a133f15a021c519950f4a07720fa0a4cb85d330f
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2010-09-14 17:36:40 +0000

    git-svn-id: http://zyc.googlecode.com/svn/lab@26 dea8d998-f866-49cc-146f-adbbfafdc004

---
+++ jee6/examples/cdi/factory/src/main/java/examples/AuditServlet.java
@@ -0,0 +1,10 @@
+package examples;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/")
+public class AuditServlet extends HttpServlet {
+	
+}
