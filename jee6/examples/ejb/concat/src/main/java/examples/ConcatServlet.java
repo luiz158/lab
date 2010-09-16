be710dbb80bbf392beff6a05c4fb00808a3c8fa7
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2010-09-16 16:57:45 +0000

    git-svn-id: http://zyc.googlecode.com/svn/lab@42 dea8d998-f866-49cc-146f-adbbfafdc004

--- jee6/examples/ejb/concat/src/main/java/examples/ConcatServlet.java
+++ jee6/examples/ejb/concat/src/main/java/examples/ConcatServlet.java
@@ -13,16 +13,16 @@ import javax.servlet.http.HttpServlet;
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/")
 public class ConcatServlet extends HttpServlet {
-	
+
 	@EJB
 	private ConcatBean concatBean;
-	
+
 	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-		String result = concatBean.concat(request.getProtocol(), request.getRemoteHost(), new Date().toString());
-		response.getWriter().write("Resultado: " + result.toString());
-		
-		super.service(request, response);
+	public void service(ServletRequest request, ServletResponse response)
+			throws ServletException, IOException {
+		String result = concatBean.concat(request.getProtocol(),
+				request.getRemoteHost(), new Date().toString());
+		response.getWriter().write("Resultado: " + result);
 	}
-	
+
 }
