052b06607f915706a71fa707581b8fb6e734cd43
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-05-18 07:53:36 -0300

    Preparação do projeto

---
+++ ios/dojo/serpro/Inscricao/Inscricao/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Inscricao
+//
+//  Created by Cleverson Sacramento on 18/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
