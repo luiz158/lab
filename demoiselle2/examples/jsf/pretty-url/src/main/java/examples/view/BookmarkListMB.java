13b35e0fb6307ae0e4bfe6908a8fcfffcc209cd4
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-07-03 23:21:04 -0300

    Novo projeto de exemplo: Pretty URL com JSF e Demoiselle 2

---
+++ demoiselle2/examples/jsf/pretty-url/src/main/java/examples/view/BookmarkListMB.java
@@ -0,0 +1,47 @@
+package examples.view;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.annotation.NextView;
+import br.gov.frameworkdemoiselle.annotation.PreviousView;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import br.gov.frameworkdemoiselle.template.AbstractListPageBean;
+import br.gov.frameworkdemoiselle.transaction.Transactional;
+
+import examples.business.BookmarkBC;
+import examples.domain.Bookmark;
+
+@ViewController
+@NextView("pretty:bookmark-edit")
+@PreviousView("pretty:bookmark-list")
+public class BookmarkListMB extends AbstractListPageBean<Bookmark, Long> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private BookmarkBC bc;
+
+	@Override
+	protected List<Bookmark> handleResultList() {
+		return this.bc.findAll();
+	}
+
+	@Transactional
+	public String deleteSelection() {
+		boolean delete;
+		for (Iterator<Long> iter = getSelection().keySet().iterator(); iter.hasNext();) {
+			Long id = iter.next();
+			delete = getSelection().get(id);
+
+			if (delete) {
+				bc.delete(id);
+				iter.remove();
+			}
+		}
+		return getPreviousView();
+	}
+
+}
