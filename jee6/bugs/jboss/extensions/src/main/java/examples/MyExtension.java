9257ae0d65f5ce8fa0d0ace72764a49b307cf433
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-07-19 14:33:26 -0300

    Implementação do teste.

--- jee6/bugs/jboss/extensions/src/main/java/examples/MyExtension.java
+++ jee6/bugs/jboss/extensions/src/main/java/examples/MyExtension.java
@@ -1,13 +1,23 @@
 package examples;
 
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 
 public class MyExtension implements Extension {
 
-	public <T> void processAnnotatedType(@Observes final ProcessAnnotatedType<T> event, final BeanManager beanManager) {
-		System.out.println("processing annoted type " + event.getAnnotatedType());
+	public void starting(@Observes final BeforeBeanDiscovery event) {
+		System.out.println("starting");
+	}
+
+	public <T> void processing(@Observes final ProcessAnnotatedType<T> event) {
+		System.out.println("processing annoted type "
+				+ event.getAnnotatedType());
+	}
+
+	public void started(@Observes final AfterDeploymentValidation event) {
+		System.out.println("started");
 	}
 }
