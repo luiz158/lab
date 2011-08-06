a8011ceab3c6668de751369d35a1d23083698376
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-08-06 17:39:01 -0300

    Finalização da aplicação de exemplo Demoiselle + Rasea

--- demoiselle2/examples/security/jsf-rasea/src/main/java/examples/view/BookmarkEditMB.java
+++ demoiselle2/examples/security/jsf-rasea/src/main/java/examples/view/BookmarkEditMB.java
@@ -3,6 +3,7 @@ package examples.view;
 import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
+import br.gov.frameworkdemoiselle.security.RequiredPermission;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
@@ -21,6 +22,7 @@ public class BookmarkEditMB extends AbstractEditPageBean<Bookmark, Long> {
 
 	@Override
 	@Transactional
+	@RequiredPermission(resource = "favoritos", operation = "excluir")
 	public String delete() {
 		this.bookmarkBC.delete(getId());
 		return getPreviousView();
@@ -28,6 +30,7 @@ public class BookmarkEditMB extends AbstractEditPageBean<Bookmark, Long> {
 
 	@Override
 	@Transactional
+	@RequiredPermission(resource = "favoritos", operation = "inserir")
 	public String insert() {
 		this.bookmarkBC.insert(getBean());
 		return getPreviousView();
@@ -35,12 +38,14 @@ public class BookmarkEditMB extends AbstractEditPageBean<Bookmark, Long> {
 
 	@Override
 	@Transactional
+	@RequiredPermission(resource = "favoritos", operation = "atualizar")
 	public String update() {
 		this.bookmarkBC.update(getBean());
 		return getPreviousView();
 	}
 
 	@Override
+	@RequiredPermission(resource = "favoritos", operation = "vizualizar")
 	protected void handleLoad() {
 		setBean(this.bookmarkBC.load(getId()));
 	}
